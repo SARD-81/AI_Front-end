62c4f89bf44e378d4c32e67de3bbae1fd34976cc
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-06-14 12:28:18 +0330

    Fix global not-found messages type

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
-import {NextIntlClientProvider} from 'next-intl';
+import {NextIntlClientProvider, type AbstractIntlMessages} from 'next-intl';
 import faMessages from '@/messages/fa.json';
 import {NotFoundView} from '@/components/errors/NotFoundView';
 
 export default function GlobalNotFoundPage() {
   return (
-    <NextIntlClientProvider locale="fa" messages={faMessages}>
+    <NextIntlClientProvider locale="fa" messages={faMessages as unknown as AbstractIntlMessages}>
       <NotFoundView locale="fa" />
     </NextIntlClientProvider>
   );
